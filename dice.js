506cdd1cecce3e687433f6fe5b721eec13bfbc61
Carolynn Kohler <ckohler@gogsf.com>
2019-10-15 14:55:13 -0500

    correct my switch statement to remove the switches and started working on function to tell how many sided dice to use

--- dice.js
+++ dice.js
@@ -5,76 +5,74 @@ function shakeDice(diceSize){
 }
 
 function runGame(){
-	let roll = shakeDice(6);
 	let previouslyRolled = [];
-	switch (roll){
-		case 1:
-		console.log(1);
-		previouslyRolled.push(1);
-		break;
+	for(let i=0; i<=5;i++){
+		let roll = shakeDice(11);
+		switch (roll){
+			case 1:
+				console.log(1);
+				previouslyRolled.push(1);
+				break;
+			case 2:
+				console.log(2);
+				previouslyRolled.push(2);
+				break;
+			case 3:
+				console.log(3);
+				previouslyRolled.push(3);
+				break;
+			case 4:
+				console.log(4);
+				previouslyRolled.push(4);
+				break;
+			case 5:
+				console.log(5);
+				previouslyRolled.push(5);
+				break;
+			case 6:
+				console.log(6);
+				previouslyRolled.push(6);
+				break;
+			case 7:
+				console.log("Wild!");
+				previouslyRolled.push(prompt("What number do you choose?"));
+				break;
+			case 8:
+				console.log("Blank");
+				previouslyRolled.push(undefined);
+				break;
+			case 9:
+				console.log("Roll Twice!");
+				previouslyRolled.push("Roll Twice");
+				break;
+			case 10:
+				console.log("Remove One Value");
+				previouslyRolled.pop();
+				break;
+			case 11:
+				console.log("Start Over!");
+				previouslyRolled.splice(0,6);
+				break;
+		}
+		console.log(previouslyRolled)
 	}
-	switch (roll){
-		case 2:
-		console.log(2);
-		previouslyRolled.push(2);
-		break;
-	}
-	switch (roll){
-		case 3:
-		console.log(3);
-		previouslyRolled.push(3);
-		break;
-	}
-	switch (roll){
-		case 4:
-		console.log(4);
-		previouslyRolled.push(4);
-		break;
-	}
-	switch (roll){
-		case 5:
-		console.log(5);
-		previouslyRolled.push(5);
-		break;
-	}
-	switch (roll){
-		case 6:
-		console.log(6);
-		previouslyRolled.push(6);
-		break;
-	}
-	switch (roll){
-		case 7:
-		console.log("Wild!");
-		previouslyRolled.push("Wild");
-		break;
-	}
-	switch (roll){
-		case 8:
-		console.log("Blank");
-		previouslyRolled.push("Blank");
-		break;
-	}
-	switch (roll){
-		case 9:
-		console.log("Roll Twice!");
-		previouslyRolled.push("Roll Twice");
-		break;
-	}
-	switch (roll){
-		case 10:
-		console.log("Remove One Value");
-		previouslyRolled.push("Remove One Value");
-		break;
-	}
-	switch (roll){
-		case 11:
-		console.log("Start Over!");
-		previouslyRolled.push("Start Over!");
-		break;
-	}
-	console.log(previouslyRolled)
 }
 
+function howManySidesToUse(){
+	if(previouslyRolled.length = 0){
+		diceSize = 6;
+	} else if(previouslyRolled.length = 1){
+		diceSize = 7;
+	} else if(previouslyRolled.length = 2){
+		diceSize = 8;
+	} else if(previouslyRolled.length = 3){
+		diceSize = 9;
+	} else if(previouslyRolled.length = 4){
+		diceSize = 10;
+	} else {
+		diceSize = 11;
+	}
+
+}
 
 runGame();
